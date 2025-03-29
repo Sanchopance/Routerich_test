9db2aa8f0ab23bad501cd66fa3cc10ec89256625
Sanchopance <_00w00_@mail.ru>
2025-03-29 08:31:00 +0300

    Update configure_zaprets.sh

--- configure_zaprets.sh
+++ configure_zaprets.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-URL="https://raw.githubusercontent.com/Sanchopance/Routerich/refs/heads/main"
+URL="https://raw.githubusercontent.com/Sanchopance/Routerich_test/refs/heads/main"
 DIR="/etc/config"
 DIR_BACKUP="/root/backup"
 config_files="dhcp
